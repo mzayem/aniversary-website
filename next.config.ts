239d45f61d7cd2c46821a910685693e39db59501
mzayem <mzayemazam@gmail.com>
2026-09-12 23:47:56 +0500

    env setup

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+    qualities: [75, 90, 95, 100],
+  },
+};
+
+export default nextConfig;
